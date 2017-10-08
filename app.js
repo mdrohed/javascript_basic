8a16e50fb9d767abc0b9f29d8e108d15c8fe9539
Md Rohed <darkrohed24@gmail.com>
2017-10-08 16:49:30 +0600

    new commits

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 var myFirstName = 'sabit';
 var lastName = 'hossain';
+var array = [1,2,3,4,5,6,7];
+//console.log(`My Name is ${myFirstName} ${lastName}`); //template literal
 
-console.log(`My Name is ${myFirstName} ${lastName}`); //template literal
+//console.log(` 1 + 1 = ${1+1}`);
 
-console.log(` 1 + 1 = ${1+1}`);
+function greet ( name) {
+    var greetings = 'Hi!';    
+    console.log(`Hello ${name}! how are you?`);
+}
+
+
+for( let i in array){
+    console.log(i);
+}
+
+console.log('Hurray javascript is cool');
+
+console.log(array);
